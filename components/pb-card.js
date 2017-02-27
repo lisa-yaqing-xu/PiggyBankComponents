86ab11d7b8989c11498917a08142e05b13c7f144
Lisa Xu <xuvya02@ca.com>
2017-02-27 15:16:38 -0500

    simplified card logic

--- components/pb-card.js
+++ components/pb-card.js
@@ -3,40 +3,19 @@
 
     let polymerConfig = {
         is: 'pb-card',
-        behaviors: [
-            Polymer.IronResizableBehavior
-        ],
         properties: {
-            label: {
-                type: String
-            },
             cost: {
                 type: Number,
-                observer: 'onCostChange',
                 notify: true
             },
-            _formattedCost: {
-                type: String
+            label: {
+                type: String,
+                notify: true
             }
         },
-        onCostChange: function(newCost) {
-            this._formattedCost = this._convertToCurrency(newCost);
-        },
-        _convertToCurrency: function(num) {
-            let numStr = num.toString();
-            let formattedNumber = "";
-            let count = 0;
-            for (let i = numStr.length - 1; i >= 0; i--) {
-                if (count === 3) {
-                    formattedNumber = numStr[i] + "," + formattedNumber;
-                    count = 1;
-                } else {
-                    formattedNumber = numStr[i] + formattedNumber;
-                    count++;
-                }
-            }
-            formattedNumber = "$" + formattedNumber;
-            return formattedNumber;
+        displayCost: function (cost) {
+            let commaStr = cost.toLocaleString(undefined, { minimumFractionDigits: 0 });
+            return `$${commaStr}`;
         }
     };
     Polymer(polymerConfig);
