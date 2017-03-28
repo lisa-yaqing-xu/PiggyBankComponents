c72b5ec78ab50adefc25754b2795d1311b0be3a6
Lisa Xu <xuvya02@ca.com>
2017-03-28 17:02:52 -0400

    calendar year and month shift calculated

--- components/pb-calendar.js
+++ components/pb-calendar.js
@@ -5,6 +5,8 @@
         constructor() {
             super();
             this.timestampDay = 1000 * 60 * 60 * 24; //ms * s * m * h
+            this.monthStr = ['January', 'Feburary', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December'];
         }
 
         static get is() {
@@ -34,22 +36,50 @@
                     type: Object,
                     notify: true,
                     value: { row: -1, col: -1 }
-                }
+                },
+                monthSelectionDate: {
+                    type: Object,
+                    notify: true
+                },
+                isDateSelectedThroughClick: Boolean
             }
+        }
+        static get observers() {
+            return [
+                '_updateSelectedDate(selectedDate)',
+                '_updateMonth(monthTimestamp)'
+            ]
+        }
+
+
+        /*
+            FUNCTIONS
+        */
+
+        getYear(date) {
+            return date.getFullYear();
+        }
+        getMonth(date) {
+            return this.monthStr[date.getMonth()];
         }
 
         getCalendarVal(dateObj) {
             return dateObj ? dateObj.getDate() : null;
         }
-
-        /*
-            FUNCTIONS
-        */
-        connectedCallback() {
-            Polymer.RenderStatus.beforeNextRender(this, function () {
-                let { startDate, endDate } = this.calculateMonthBound(this.monthTimestamp)
-                this.dates = this.calculateCalendarBox(startDate, endDate)
-            });
+        getSelectionIndex(dateObj) {
+            if (!dateObj) {
+                return { row: -1, col: -1 };
+            } else {
+                let col = dateObj.getDay();
+                let row = 0;
+                let date = dateObj.getDate();
+                while (date > 7) {
+                    date -= 7;
+                    row++;
+                }
+                if ((date - 1) > col) row++;
+                return { row, col };
+            }
 
         }
 
@@ -76,6 +106,7 @@
 
         onDateSelect(event) {
             let { target, row, col, date } = this.getDateInfoFromEventTarget(event, this.dates);
+            this.isDateSelectedThroughClick = true;
             if (date) {
                 this.set('selectedDateIndex', { row: row, col: col });
                 this.set('selectedDate', date);
@@ -84,6 +115,7 @@
                 this.set('selectedDateIndex', { row: -1, col: -1 });
                 this.set('selectedDate', null);
             }
+
         }
         getCalendarClass(date, selectedDate) {
             if (!date) {
@@ -93,13 +125,96 @@
                 return this.isSameDate(date, selectedDate) ? 'selected' : ''
             }
         }
+
+        incrementMonth(){
+            this.monthTimestamp = this.calculateMonthChange(this.monthSelectionDate, 1).getTime();
+        }
+
+        decrementMonth(){
+            this.monthTimestamp = this.calculateMonthChange(this.monthSelectionDate, -1).getTime();
+        }
+
+        incrementYear(){
+            this.monthTimestamp = this.calculateMonthChange(this.monthSelectionDate, 12).getTime();
+        }
+
+        decrementYear(){
+            this.monthTimestamp = this.calculateMonthChange(this.monthSelectionDate, -12).getTime();
+        }
+
+        calculateMonthChange(monthData, amount){
+            let year = monthData.getFullYear();
+            let month = monthData.getMonth();
+            let numShift = Math.ceil(Math.abs(amount/12)) * 12;
+
+            let newMonth = (month + numShift + amount) % 12;
+            let newYear = year;
+            if(amount > 0) newYear += Math.floor((month + amount) / 12);
+            else if (amount < 0) newYear -= Math.floor((month + amount - 11)/-12)
+            return new Date(newYear, newMonth, 1);
+        }
+
+
         /*
             OBSERVERS
         */
 
-        monthChanged(newMonth, oldMonth) {
-            //TODO: implement
+        _updateMonth(monthTimestamp) {
+            let { startDate, endDate, monthSelectionDate } = this.calculateMonthBound(monthTimestamp);
+            this.monthSelectionDate = monthSelectionDate;
+            this.dates = this.calculateCalendarBox(startDate, endDate);
+            if (this.selectedDate) {
+                // check if date is in this month and year
+                let month = this.selectedDate.getMonth();
+                let year = this.selectedDate.getFullYear();
+                let thisMonth = this.monthSelectionDate.getMonth();
+                let thisYear = this.monthSelectionDate.getFullYear();
+
+                if (month === thisMonth && year === thisYear) {
+                    // if so, update indices
+                    this.selectedDateIndex = this.getSelectionIndex(this.selectedDate)
+                } else {
+                    // else update date
+                    // don't also update the selected date index 
+                    // because this will trigger date update selectionDateIndex
+                    let day = Math.min(this.selectedDate.getDate(), this.getNumDaysInMonth(thisMonth, thisYear));
+                    this.selectedDate = new Date(thisYear, thisMonth, day);
+                }
+
+            }
         }
+
+        _updateSelectedDate(selectedDate) {
+            if (!selectedDate) {
+                this.selectionDateIndex = this.getSelectionIndex(selectedDate);
+                return;
+            }
+
+            if (this.isDateSelectedThroughClick) {
+                this.isDateSelectedThroughClick = false;
+                return;
+            }
+
+            let selectedMonth = selectedDate.getMonth();
+            let selectedYear = selectedDate.getFullYear();
+            let displayMonth = this.monthSelectionDate.getMonth();
+            let displayYear = this.monthSelectionDate.getFullYear();
+            let sameMonth = selectedMonth === displayMonth;
+            let sameYear = selectedYear === displayYear;
+
+            let newMonthDateObj = new Date(this.monthSelectionDate);
+            if (!sameMonth) newMonthDateObj.setMonth(selectedMonth);
+            if (!sameYear) newMonthDateObj.setFullYear(selectedYear);
+
+            let newMonthTimeStamp = newMonthDateObj.getTime();
+
+            if (this.monthTimestamp === newMonthTimeStamp) {
+                this.selectionDateIndex = this.getSelectionIndex(selectedDate);
+            } else {
+                this.monthTimestamp = newMonthTimeStamp;
+            }
+        }
+
 
         /*
             HELPER FUNCTIONS
@@ -117,7 +232,7 @@
             let initialMonthDate = new Date(initDateYear, initDateMonth, 1);
             let endMonthDate = new Date(initDateYear, initDateMonth, numOfDaysInMonth);
 
-            return { startDate: initialMonthDate, endDate: endMonthDate };
+            return { startDate: initialMonthDate, endDate: endMonthDate, monthSelectionDate: initDate };
         }
 
         calculateCalendarBox(startDate, endDate) {
