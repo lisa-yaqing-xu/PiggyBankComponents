5244cd22e2cdc0d4068ce67a48a9f5c52b23df37
Mistry, Sunny <Sunny.Mistry@ca.com>
2017-03-29 12:07:36 -0400

    pb-project-details component created

---
+++ components/pb-project-details.js
@@ -0,0 +1,18 @@
+(function () {
+    class PBProjectDetails extends Polymer.Element {
+
+      static get is() { return 'pb-project-details'; }
+
+      static get properties() {
+        return {
+        }
+      }
+
+      constructor() {
+        super();
+      }
+      
+    }
+    
+    customElements.define(PBProjectDetails.is, PBProjectDetails);
+})();
